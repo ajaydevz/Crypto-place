cc5e044ea3f17d27b9e1a81b2296a4e5c0718fc0
ajaydev <ajaydevzzz30@gmail.com>
2025-04-27 16:34:17 +0530

    fix: update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow_icon from '../../assets/arrow_icon.png'
 import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
    
@@ -31,9 +32,12 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-        <img src={logo} alt="" />
+        <Link to={'/'}>
+            <img src={logo} alt="" />
+        </Link>
+        
         <ul>
-            <li>Home</li>
+            <Link to={'/'}> <li>Home</li></Link>
             <li>Features</li>
             <li>Pricing</li>
             <li>Blog</li>
